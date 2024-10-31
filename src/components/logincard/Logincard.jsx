b4104d42b95b07ed5da18fa0b72bd8e5d59b4719
Blackeye6941 <leok37316@gmail.com>
2024-10-31 22:25:01 +0530

    Login styles updated

--- src/components/logincard/Logincard.jsx
+++ src/components/logincard/Logincard.jsx
@@ -3,7 +3,7 @@
 const Logincard = () => {
   return (
     <>
-      <div className="flex flex-col bg-slate-300 text-black max-md:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[40%] h-[51vh] mb-[20vh] mx-auto rounded-[40px]">
+      <div className="flex flex-col bg-slate-300 text-black max-md:w-[50vh] md:w-[50%] lg:w-[40%] xl:w-[40%] h-[51vh] mb-[20vh] mx-auto rounded-[40px]">
         <div className="text-[40px] mx-auto pt-4 pb-10">Login</div>
         <form action="">
           <div className=" max-md:px-[25px] md:px-[40px] lg:px-[50px] xl:px-[100px] pb-10"><label htmlFor="Username" className="text-[20px]">Username: </label>
@@ -12,7 +12,7 @@ const Logincard = () => {
           <div className=" max-md:px-[25px] md:px-[40px] lg:px-[50px] xl:px-[100px] pb-6"><label htmlFor="Password" className="text-[20px]">Password: </label>
             <input className="text-black border-0 rounded-lg" type="password" id="name" placeholder="Enter Password"/>
             </div>
-          <button className="bg-black text-white text-[20px] px-6 max-md:mx-[70px] md:mx-[140px] lg:mx-[155px] xl:mx-[220px] rounded-xl py-2 ">Login</button>
+          <button className="bg-black text-white text-[20px] px-6 max-md:mx-[135px] md:mx-[140px] lg:mx-[155px] xl:mx-[220px] rounded-xl py-2 ">Login</button>
         </form>
       </div>
     </>
