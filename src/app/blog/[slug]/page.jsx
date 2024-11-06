1f9ff7af2dea7f7000e9eae13abb4045d62b21f7
Blackeye6941 <leok37316@gmail.com>
2024-11-06 23:35:47 +0530

    Single Blog page Update

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,28 @@
+import Image from "next/image"
 
 const SingleBlogPage = () => {
   return (
-    <div>
-      Single Blog Page
-    </div>
+    <>
+      <div className="flex gap-2 lg:gap-[40px] xl:gap-[50px] flex-col sm:flex-row">
+        <div className="flex relative w-[100%] max-md:w-[60%] lg:w-[60%] xl:w-[40%] h-[35vh] max-md:h-[35vh] md:h-[65vh] lg:h-[65vh] xl:h-[65vh] translate-y-[-50px]">
+          <Image src="/image1.jpg" alt="" fill className="object-cover"/>
+        </div>
+        <div className="flex-2">
+          <h1 className="text-[45px]">Title</h1>
+          <div className="flex flex-col gap-3 text-[20px] border-b-2">
+          <div className="flex flex-row gap-10">
+            <span>Author</span>
+            <span>Date</span>
+          </div>
+          <div className="flex flex-row gap-10">
+            <span>Michel</span>
+            <span>22.12.2005</span>
+          </div>
+          </div>
+          <p className="pt-8  text-[14px] ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, temporibus illum mollitia facilis eius dolorum odit laborum, architecto consequuntur iusto doloribus aliquid dicta iste soluta. Aperiam illum cumque iusto reiciendis?</p>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/blogcard/BlogCard.jsx
+++ src/components/blogcard/BlogCard.jsx
@@ -6,15 +6,15 @@ const BlogCard = () => {
     <>
         <div className="flex flex-col gap-[20px] mb-3">
             <div className="flex">
-            <div className="relative w-[90%] h-[250px]">
-                <Image src="/image1.jpg" alt="" fill className="object-cover"/>
-            </div>
-            <span className=" text-[14px] rotate-[270deg] m-auto">1.01.2005</span>
+                <div className="relative w-[90%] h-[250px]">
+                    <Image src="/image1.jpg" alt="" fill className="object-cover"/>
+                </div>
+                <span className=" text-[14px] rotate-[270deg] m-auto">1.01.2005</span>
         </div>
             <div className="">
                 <h1 className="text-[20px]">Title</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non earum a assumenda consectetur quaerat et minima, harum nihil pariatur quisquam at amet minus quae qui, maxime ex aut deleniti soluta.</p>
-            <Link href="" className="text-blue-600 underline">Read more.....</Link>
+            <Link href="/blog/post" className="text-blue-600 underline">Read more.....</Link>
             </div>
         </div>
     </>
